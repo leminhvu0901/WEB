6eaeb2c40412426ed85ff1094999190618dab33f
Le Minh Vu <leminhvu9124@gmail.com>
2026-03-13 12:09:26 +0700

    cap nhat them xoa sua users

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,5 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\FollowController;
-use App\Http\Controllers\PostCommentController;
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\PostLikeController;
-use App\Http\Controllers\PostShareController;
-use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -20,39 +13,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
-
-//REGISTER ANH LOGIN
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-
-//USERS
+// USERS
 Route::get('/users', [UserController::class, 'index']);
 Route::get('/users/{user}', [UserController::class, 'show']);
-Route::get('/users/{user}/posts', [UserController::class, 'posts']);
-Route::get('/users/{user}/followers', [UserController::class, 'followers']);
-Route::get('/users/{user}/following', [UserController::class, 'following']);
-
-
-Route::get('/products', [ProductController::class, 'index']);
-Route::get('/products/{product}', [ProductController::class, 'show']);
-
-Route::get('/posts', [PostController::class, 'index']);
-Route::get('/posts/{post}', [PostController::class, 'show']);
-
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/me', [AuthController::class, 'me']);
-    Route::match(['put', 'patch'], '/me', [AuthController::class, 'updateProfile']);
-
-    Route::post('/posts', [PostController::class, 'store']);
-    Route::match(['put', 'patch'], '/posts/{post}', [PostController::class, 'update']);
-    Route::delete('/posts/{post}', [PostController::class, 'destroy']);
-
-    Route::post('/posts/{post}/likes', [PostLikeController::class, 'toggle']);
-    Route::post('/posts/{post}/comments', [PostCommentController::class, 'store']);
-    Route::delete('/comments/{comment}', [PostCommentController::class, 'destroy']);
-    Route::post('/posts/{post}/shares', [PostShareController::class, 'store']);
-
-    Route::post('/users/{user}/follow', [FollowController::class, 'toggle']);
-});
+Route::post('/users', [UserController::class, 'store']);
+Route::match(['put', 'patch'], '/users/{user}', [UserController::class, 'update']);
+Route::delete('/users/{user}', [UserController::class, 'destroy']);

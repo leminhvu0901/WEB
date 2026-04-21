e07c63d2244c216171514dac7d87e8c28aca4c31
Le Minh Vu <leminhvu9124@gmail.com>
2026-04-21 14:57:00 +0700

    add PUT/PATCH route for updating posts

--- routes/api.php
+++ routes/api.php
@@ -33,8 +33,7 @@ Route::get('/users/{user}/posts', [PostController::class, 'byUser']); // xem tâ
 Route::middleware('auth:sanctum')->group(function () {
 	Route::get('/me', [AuthController::class, 'me']); //thong tin user hien tai dang dang nhap
 
-	Route::post('/posts', [PostController::class, 'add']); // tạo bài viết mới (nhiều ảnh)
-	Route::delete('/posts/{post}', [PostController::class, 'destroy']); // xóa bài viết
+	Route::post('/posts', [PostController::class, 'add']); // tạo bài viết mới (nhiều ảnh)	Route::match(['put', 'patch'], '/posts/{post}', [PostController::class, 'update']); // chỉnh sửa bài viết	Route::delete('/posts/{post}', [PostController::class, 'destroy']); // xóa bài viết
 
 });
 
